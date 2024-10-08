cdc8abd8f0208cb57b268b7cefca11dc02788464
Cyncrovee <86919717+Cyncrovee@users.noreply.github.com>
2024-10-08 23:02:29 +0100

    Added file picker and calculate button

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,6 +1,10 @@
 using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Avalonia.Platform.Storage;
 
 namespace CynVee_Hash_Checker;
 
@@ -9,6 +13,22 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    string filePath = null;
+    private async void selectFileButton_Click(object sender, RoutedEventArgs e)
+    {
+        var topLevel = TopLevel.GetTopLevel(this);
+        var file = await topLevel.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+        {
+            Title = "Open Text File",
+            AllowMultiple = false
+        });
+
+        if (file.Count >= 1)
+        {
+            filePath = file.First().Path.ToString();
+        }
     }
 
     private void sha1Button_Click(object sender, RoutedEventArgs e)
@@ -26,5 +46,16 @@ public partial class MainWindow : Window
     private void md5Button_Click(object sender, RoutedEventArgs e)
     {
         md5TextBox.Paste();
+    }
+
+    private void calculateButton_Click(object sender, RoutedEventArgs e)
+    {
+        using (var md5 = MD5.Create())
+        {
+            using (var stream = File.OpenRead(filePath))
+            {
+                Console.WriteLine("temp");
+            }
+        }
     }
 }
