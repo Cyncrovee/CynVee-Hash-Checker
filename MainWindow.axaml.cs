8f1930cc0b62b5a27b34b909534bbb32271d40b5
Cyncrovee <86919717+Cyncrovee@users.noreply.github.com>
2024-10-10 17:34:30 +0100

    Added the funtionality to compare calculated hashes to to input hash

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,11 +1,15 @@
 using System;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
 using Avalonia.Interactivity;
+using Avalonia.Media;
 using Avalonia.Platform.Storage;
+using Color = Avalonia.Media.Color;
 
 namespace CynVee_Hash_Checker;
 
@@ -28,8 +32,8 @@ public partial class MainWindow : Window
 
         if (file.Count >= 1)
         {
-            filePath = file.First().Path.PathAndQuery;
-            Console.WriteLine(filePath);
+            filePath = file.First().Path.AbsolutePath;
+            filePathBlock.Text = "Currently Selected File: " + filePath;
         }
     }
 
@@ -56,6 +60,63 @@ public partial class MainWindow : Window
         var sha256String = calculateSHA256();
         var sha512String = calculateSHA512();
         var md5String = calculateMD5();
+
+        if (sha1TextBox.Text != null)
+        {
+            bool sha1Result = sha1TextBox.Text.Equals(sha1String);
+            if (sha1Result == true)
+            {
+                sha1ResultBlock.Text = "Result: True- Hashes match! Value: " + sha1String;
+                sha1ResultBlock.Foreground = Brushes.LightGreen;
+            }
+            else
+            {
+                sha1ResultBlock.Text = "Result: False- Hashes dont match! Actual value: " + sha1String;
+                sha1ResultBlock.Foreground = Brushes.Crimson;
+            }
+        }
+        if (sha256TextBox.Text != null)
+        {
+            bool sha256Result = sha256TextBox.Text.Equals(sha256String);
+            if (sha256Result == true)
+            {
+                sha256ResultBlock.Text = "Result: True- Hashes match! Value: " + sha256String;
+                sha256ResultBlock.Foreground = Brushes.LightGreen;
+            }
+            else
+            {
+                sha256ResultBlock.Text = "Result: False- Hashes dont match! Actual value: " + sha256String;
+                sha256ResultBlock.Foreground = Brushes.Crimson;
+            }
+        }
+        if (sha512TextBox.Text != null)
+        {
+            bool sha512Result = sha512TextBox.Text.Equals(sha512String);
+            if (sha512Result == true)
+            {
+                sha512ResultBlock.Text = "Result: True- Hashes match! Value: " + sha512String;
+                sha512ResultBlock.Foreground = Brushes.LightGreen;
+            }
+            else
+            {
+                sha512ResultBlock.Text = "Result: False- Hashes dont match! Actual value: " + sha512String;
+                sha512ResultBlock.Foreground = Brushes.Crimson;
+            }
+        }
+        if (md5TextBox.Text != null)
+        {
+            bool md5Result = md5TextBox.Text.Equals(md5String);
+            if (md5Result == true)
+            {
+                md5ResultBlock.Text = "Result: True- Hashes match! Value: " + md5String;
+                md5ResultBlock.Foreground = Brushes.LightGreen;
+            }
+            else
+            {
+                md5ResultBlock.Text = "Result: False- Hashes dont match! Actual value: " + md5String;
+                md5ResultBlock.Foreground = Brushes.Crimson;
+            }
+        }
     }
 
     public string calculateSHA1()
